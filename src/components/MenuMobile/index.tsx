7dec9e91ef4506b14a77a74c2a5f691b84a034f3
Carlos Daniel Rodrgues <101821262+NielRodrigues@users.noreply.github.com>
2023-04-18 11:31:24 -0300

    icons menu 2

--- src/components/MenuMobile/index.tsx
+++ src/components/MenuMobile/index.tsx
@@ -4,12 +4,6 @@ import {ReactComponent as Combo}  from '../../assets/image/combo.svg'
 import {ReactComponent as Vegetarian}  from '../../assets/image/vegetarian.svg'
 import {ReactComponent as Drink}  from '../../assets/image/drink.svg'
 import {ReactComponent as Desert}  from '../../assets/image/desert.svg'
-import combo from '../../assets/image/combo-icon.png'
-import vegetarian from '../../assets/image/vegetarian-icon.png'
-import drink from '../../assets/image/drink-icon.png'
-import desert from '../../assets/image/desert-icon.png'
-import {FaHamburger, FaIceCream} from 'react-icons/fa'
-import {GiFrenchFries, GiSandwich, GiSodaCan} from 'react-icons/gi'
 
 
 export default function MenuMobile() {
